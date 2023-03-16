19dc834ccc33a80f79d18ff259ace3cd52ca1bbe
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-16 12:24:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 # include <stdio.h>
-
 /**
  * main - Hello,puts
  * Return - Always 0
  */
-
-int main(void):
+int main(void)
 {
   puts("Programming is like building a multilingual puzzle");
   return (0);
