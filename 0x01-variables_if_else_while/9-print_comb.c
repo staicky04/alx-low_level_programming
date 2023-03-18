6e99a8ce3204d34c658b69e29b8f6634af24e2a2
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-17 17:24:30 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = 48; nums <= 57; nums++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
