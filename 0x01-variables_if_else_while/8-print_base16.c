296798bd49a28f54b2f86150180b164c3a71cb4e
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-17 17:14:54 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Hexadecimal
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	int alph;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(n);
+	}
+	for (alph = 97; alph <= 102; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
