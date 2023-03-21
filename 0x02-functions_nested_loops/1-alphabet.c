9277fb60d27cfc615bcca23bb4212f241b95a334
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-21 13:03:52 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,19 @@
  * print_alphabet - print all alphabet in lowercase
  */
 
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 void print_alphabet(void)
 {
 	char chars;
 
 	for (chars = 'a'; chars <= 'z'; chars++)
+	{
 		_putchar(chars);
-
-	_putchar('\n');
+	}
+	_putchar('\n');	
 }
