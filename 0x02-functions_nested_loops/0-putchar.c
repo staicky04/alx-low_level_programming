d5435fae5975d8e92f9f574321909e11ebbda677
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-21 09:56:51 -0700

    Successfully printed _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - Hello,puts
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning and practising for loops and fuctions in C programming.
