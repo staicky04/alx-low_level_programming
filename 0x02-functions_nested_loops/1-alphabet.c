091f50b37569266736aea5a246b149c1c0b1c3ef
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-21 12:56:35 -0700

    10 X alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char chars;
+
+	for (chars = 'a'; chars <= 'z'; chars++)
+		_putchar(chars);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int chars;
+	int i;
+
+	for (i = 0; i <= 10; i++
