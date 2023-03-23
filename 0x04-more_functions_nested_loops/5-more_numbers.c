f7ec8cf945b160251344a76df3c086a189e0bcfa
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-23 13:32:37 -0700

    Looping through number 0 - 9 10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - Loop through 0 to 14 10 times
+ * Return: 1 as success, 0 otherwise
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
