f654685a8c821a324bbc39800064875b94722138
Abolarin Toluwalase <staicky141@gmail.com>
2023-03-23 10:14:23 -0700

    Task #0 checking for uppercase characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isulpa - checks for uppercase character
+ * _isupper - checks for uppercase character
  * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * Return: 1 if c is a capital letter, 0 otherwise
  */
 int _isupper(int c)
 {
 	if (c >= 64 && c <= 90)
 	{
-	return (1);
+		return (1);
 	}
 	else
 	{
