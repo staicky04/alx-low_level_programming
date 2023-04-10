21377fc5ada05fdcfca90bd513981ab35782569b
Abolarin Toluwalase <staicky141@gmail.com>
2023-04-10 06:33:54 -0700

    a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
 /**
  * main - Print the name of the program
  * @argc: Count arguments
@@ -13,6 +41,7 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	count = 1;
+
 	while (count < argc)
 	{
 		if (check_num(argv[count]))
@@ -30,8 +59,7 @@ int main(int argc, char *argv[])
 		count++;
 	}
 
-	printf("%d\n", sum); /*print sum*/
+	printf("%d\n", sum);
 
 	return (0);
 }
-
