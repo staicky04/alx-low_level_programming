c5079f9d563ef44311ef2769d3a89f84f7bf818d
Abolarin Toluwalase <staicky141@gmail.com>
2023-04-10 02:43:20 -0700

    A program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - return count of arguments passed, and strings passed resp.
+ * @argc: int
+ * @argv: strings
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ARgc, ARgv, Storing the counts and also returning the string of arguments passed respetively
