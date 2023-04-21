3d65ff39102454956386686128082bb3f7f6b70f
Abolarin Toluwalase <staicky141@gmail.com>
2023-04-20 18:34:56 -0700

    worked on  a function that prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,9 +25,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
+
 		if (separator != NULL && i != (n - 1))
 		{
-			printf("%c ", *separator);
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
