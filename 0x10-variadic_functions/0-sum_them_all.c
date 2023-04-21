92e8408586d556cd9fd2ba46aada335341a81c93
Abolarin Toluwalase <staicky141@gmail.com>
2023-04-20 17:43:07 -0700

    Now with variadic function, it saves time on argument  pass on my called function .

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - sums all the arguments passed
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
-#ifndef Var_Func
-#define Var_Func
-#include <stdarg.h>
+#ifndef VAR_FUNC
+#define VAR_FUNC
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
