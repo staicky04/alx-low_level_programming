0c33170077e04daff18b9409c7c6fe8a7dbc3609
Abolarin Toluwalase <staicky141@gmail.com>
2023-05-03 07:04:21 -0700

    a function that adds a new node at the end of a

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	else
 		return (NULL);
-	if (point != NULL)
+	if (temp != NULL)
 	{
 		while (temp->next != NULL)
 			temp = temp->next;
